dff8aeb1f927fbd9b2440636aa0984659d53a559
DHARSHAN KUMAR K <k.dharshan7135@gmail.com>
2026-04-06 18:53:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 
   preview: {
-    allowedHosts: "all"
+    allowedHosts: [
+      "velson-backend.onrender.com",
+      "velson-backend-72n8.onrender.com"
+    ]
+  },
+
+  server: {
+    host: true,
+    port: 5173
   }
 })
