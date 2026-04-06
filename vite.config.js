677d0964863af8deb2249acc28819e5c335d1d7c
DHARSHAN KUMAR K <k.dharshan7135@gmail.com>
2026-04-06 17:52:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+
   plugins: [react(), tailwindcss()],
+
+  // for local development
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Express API server
-        changeOrigin: true,
-      },
-    },
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
   },
+
+  // for render deployment
+  preview: {
+    host: true,
+    port: process.env.PORT || 10000,
+    allowedHosts: "all",
+
+    proxy: {
+      '/api': {
+        target: 'https://velson-backend.onrender.com',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
+
 })
